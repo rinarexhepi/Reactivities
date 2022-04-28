c6c320ac2dc01c7627ad8f72ee5257ef2fbf008c
rinarexhepi <rinnaarexhepi@gmail.com>
2022-04-28 14:44:11 +0200

    helloo

---
+++ Application/Country/Delete.cs
@@ -0,0 +1,35 @@
+﻿
+using MediatR;
+using Persistence;
+
+namespace Application.Country
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public int ID_Shteti { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var book=await _context.Shteti.FindAsync(request.ID_Shteti);
+
+                _context.Remove(book);
+
+                await _context.SaveChangesAsync(); 
+
+                return Unit.Value;
+            }
+        }
+    }
+}
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,5 +11,6 @@ namespace Persistence
 
         public DbSet<Activity> Activities { get; set; } 
         public DbSet<Book> Books { get; set; }
+        public DbSet<Shteti> Shteti { get; set; }
     }
 }
