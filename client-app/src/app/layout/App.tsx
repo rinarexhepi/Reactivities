0f1847c041eeac81d6525de72ce64139eed40451
FatlindBytyqi <95626338+FatlindBytyqi@users.noreply.github.com>
2022-05-04 22:44:01 +0200

    React-app
    Added and modified src of react-app

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import LandingPage from './components/landingPage';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <LandingPage />
+    </div>
+  );
+}
+
+export default App;
